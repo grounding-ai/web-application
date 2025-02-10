70346caa2472d5566d056e4ea44ee576a626d934
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-02-10 16:36:51 +0100

    Adds labels and their summaries

---
+++ bin/build-json-summaries.ts
@@ -0,0 +1,54 @@
+import { mkdir, readFile, writeFile } from "fs/promises";
+import Papa from "papaparse";
+import path from "path";
+import { dirname } from "path";
+import { rimraf } from "rimraf";
+import { fileURLToPath } from "url";
+
+import { AnnotationContent } from "../src/core/types";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const OUTPUT_DIR = path.resolve(__dirname, "../public/summaries");
+
+async function main() {
+  await rimraf(OUTPUT_DIR);
+  await mkdir(OUTPUT_DIR);
+
+  const text = await readFile(path.resolve(__dirname, "../resources/summaries.csv"), "utf8");
+  const { data } = Papa.parse<
+    Record<"id" | "headline" | "text" | "Danish_Translation" | "english_bot_critic" | "english_bot_potential", string>
+  >(text, { header: true });
+
+  const annotations: AnnotationContent[] = data.map(
+    ({ id, headline, text, Danish_Translation, english_bot_critic, english_bot_potential }) => ({
+      id,
+      headline: {
+        en: headline,
+        da: ((Danish_Translation || "").match(/^[^:]+/) || [null])[0],
+      },
+      content: {
+        en: text,
+        da: Danish_Translation,
+      },
+      bots: {
+        critic: {
+          en: english_bot_critic || null,
+          da: null,
+        },
+        potential: {
+          en: english_bot_potential || null,
+          da: null,
+        },
+      },
+    }),
+  );
+
+  for (let i = 0; i < annotations.length; i++) {
+    const annotation = annotations[i];
+    if (annotation.id)
+      await writeFile(path.join(OUTPUT_DIR, `${annotation.id}.json`), JSON.stringify(annotation), "utf8");
+  }
+}
+
+main().catch(console.error);
--- src/core/types.ts
+++ src/core/types.ts
@@ -1,2 +1,15 @@
+export const LANGUAGES = ["en", "da"] as const;
+export type Language = (typeof LANGUAGES)[number];
+export type Translation = Record<Language, string | null>;
+
+export const BOTS = ["critic", "potential"] as const;
+export type Bot = (typeof BOTS)[number];
+
 export type Coordinates = { x: number; y: number };
 export type Annotation = { id: string; label: string; index: number } & Coordinates;
+export type AnnotationContent = {
+  id: string;
+  headline: Translation;
+  content: Translation;
+  bots: Record<Bot, Translation>;
+};
